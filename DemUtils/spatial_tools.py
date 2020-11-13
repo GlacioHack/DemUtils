f882ff242f83295508c6d17856d6729a7b9bd9aa
rhugonne <romain.hugonnet@gmail.com>
2020-11-13 13:02:36 +0100

    added rmse

--- DemUtils/spatial_tools.py
+++ DemUtils/spatial_tools.py
@@ -1,5 +1,5 @@
 """
-
+DemUtils.spatial_tools.py contains statistical tools for robust of manipulation DEM data
 """
 import numpy as np
 
@@ -8,11 +8,25 @@ def nmad(data:np.ndarray, nfact:float=1.4826)->float:
     Calculate the normalized median absolute deviation (NMAD) of an array.
 
     :param data: input data
-    :param nfact: normalization factor for the data; default is 1.4826
+    :param nfact: normalization factor for the median absolute deviation; default is 1.4826
 
     :returns nmad: (normalized) median absolute deviation of data.
     """
     m = np.nanmedian(data)
     return nfact * np.nanmedian(np.abs(data - m))
+
+
+def rmse(data:np.ndarray)->float:
+    """
+    Return root mean square of input data.
+    :param data: differences to calculate root mean square of
+
+    :returns rmse: root mean square error
+    """
+    r = np.sqrt(np.nanmean(np.asarray(data) ** 2))
+    return r
+
+
+
 
 
